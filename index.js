ebf665e5ee31cc965249ecb2aea6562d1e948be0
Davis Peck <Dpeck1997@gmail.com>
2022-04-04 11:34:04 -0700

    adding a webpack config and plugging in fibonacci's numbers

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ var moment = require("moment");
 console.log("hello");
 
 console.log(moment().format("MMM Do YY"));
+
+let fibonacci = {
+  [Symbol.iterator]() {
+    let pre = 0,
+      cur = 1;
+    return {
+      next() {
+        [pre, cur] = [cur, pre + cur];
+        return { done: false, value: cur };
+      },
+    };
+  },
+};
+
+for (var n of fibonacci) {
+  // truncate the sequence at 1000
+  if (n > 1000) break;
+  console.log(n);
+}
